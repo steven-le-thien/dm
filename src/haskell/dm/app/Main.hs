12942fd4a51c09497c2b422c9e2fdcd9104eef4b
Thien Le <thienle@Thiens-MacBook-Pro.local>
2020-10-16 15:29:51 -0400

    fix bugs with indexing at subtree leaves; test on large datasets (n = 1000)

--- src/haskell/dm/app/Main.hs
+++ src/haskell/dm/app/Main.hs
@@ -5,5 +5,7 @@ import Lib
 main :: IO()
 main = do
     n <- readLn
-    d <- distMat n
-    putStrLn (newick n (dm d n [0..(n-2)]))
+    (d,name_map) <- distMat n
+    t <- dm d n [0..(n-2)]
+    putStrLn (newick name_map n t)
+    --putStrLn (newick name_map n (dm d n [0..(n-2)]))
--- src/haskell/dm/src/Lib.hs
+++ src/haskell/dm/src/Lib.hs
@@ -6,9 +6,7 @@ import Data.List
 import Control.Monad
 
 -- Tree ADT
-data Tree = Leaf Int | Node Int Tree Tree | Error deriving Show
-
-intNode = -1
+data Tree = Leaf Int | Node Tree Tree | Error deriving Show
 
 argmin :: [Int] -> [Int]
 argmin [] = []
@@ -23,32 +21,39 @@ argmin (x:xs) = let
                         then [0]
                     else (map (+ 1) (m:ms))
 
-distMat :: Int -> IO[[Int]]
-distMat 0 = return []
+distMat :: Int -> IO([[Int]],[String])
+distMat 0 = return ([],[])
 distMat x = do
     str <- getLine
-    nextInt <- distMat (x - 1)
-    let int = map read $ words str :: [Int]
-    return (int:nextInt)
+    (next_v,next_name) <- distMat (x - 1)
+    let (name:v) = map read $ words str :: [Int]
+    return ((v:next_v),((show name):next_name))
 
 dm_vec :: [[Int]] -> Int -> Int -> Int -> Int
 dm_vec d n x y = ((d !! x) !! (n-1)) + ((d !!y) !! (n-1)) - ((d !! x) !! y)
 
-dm :: [[Int]] -> Int -> [Int] -> Tree
-dm _ _ [l]          = Leaf l
-dm _ _ [l,r]        = Node intNode (Leaf l) (Leaf r)
-dm d n (l:ls)       = Node intNode t1 t2
-                        where
-                            t1 = dm d n l1
-                            t2 = dm d n l2
+ind_all :: [Int] -> [Int] -> [Int]
+ind_all [] _ = []
+ind_all (x:xs) a = ((a !! x):(ind_all xs a))
 
-                            l1 = map (+ 1) (argmin (map (dm_vec d n l) ls))
+dm :: [[Int]] -> Int -> [Int] -> IO Tree
+dm _ _ [l]          = return (Leaf l)
+dm _ _ [l,r]        = return (Node (Leaf l) (Leaf r))
+dm d n (l:ls)       = do
+                        t1 <- dm d n l1
+                        t2 <- dm d n l2
+                        return (Node t1 t2)
+                            where
+                            --t1 <- dm d n l1
+                            --t2 <- dm d n l2
+
+                            l1 = ind_all (argmin (map (dm_vec d n l) ls)) ls
                             l2 = ((l:ls) \\ l1)
 
-newick :: Int -> Tree -> String
-newick n t = let ('(':str) = (newick' t) in "(" ++ (show (n - 1)) ++ "," ++ str ++ ";"
+newick :: [String] -> Int -> Tree -> String
+newick name_map n t = let ('(':str) = (newick' t) in "(" ++ (name_map !! (n - 1)) ++ "," ++ str ++ ";"
     where
-        newick' :: Tree->String        
-        newick' (Leaf l)        = show l
-        newick' (Node _ l r)    = "(" ++ (newick' l) ++ "," ++ (newick' r) ++ ")"
+        newick' :: Tree->String
+        newick' (Leaf l)        = name_map !! l
+        newick' (Node l r)    = "(" ++ (newick' l) ++ "," ++ (newick' r) ++ ")"
 
